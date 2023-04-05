a5fdf1f7a62e0839352fb253ab86c943d906a763
Atharv Redij <atharvaredij@gmail.com>
2023-04-05 22:44:43 +0530

    Create 0144.py

---
+++ 0144.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.traversal = [] 
+
+    def traverse(self, root):
+        if root:
+            self.traversal.append(root.val)
+            self.traverse(root.left)
+            self.traverse(root.right)
+
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.traverse(root)
+        return self.traversal
